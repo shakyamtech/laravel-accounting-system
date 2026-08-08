6dde6d6f8d2756c869780be36d7d1857d6c106be
Your Nameshakyamtech <shakya.mahes@gmail.com>
2026-08-08 11:10:24 +0545

    Fix Category model class reference in Document factory

--- database/factories/Document.php
+++ database/factories/Document.php
@@ -70,7 +70,7 @@ class Document extends AbstractFactory
             $statuses = ['draft', 'sent', 'viewed', 'partial', 'paid', 'cancelled'];
 
             $income_categories = $this->company->categories()->income()->get();
-            $income_category_id = $income_categories->count() ? $income_categories->random(1)->pluck('id')->first() : Category::factory()->income()->create(['company_id' => $this->company->id])->id;
+            $income_category_id = $income_categories->count() ? $income_categories->random(1)->pluck('id')->first() : \App\Models\Setting\Category::factory()->income()->create(['company_id' => $this->company->id])->id;
 
             return [
                 'type' => Model::INVOICE_TYPE,
@@ -104,7 +104,7 @@ class Document extends AbstractFactory
             $statuses = ['draft', 'received', 'partial', 'paid', 'cancelled'];
 
             $expense_categories = $this->company->categories()->expense()->get();
-            $expense_category_id = $expense_categories->count() ? $expense_categories->random(1)->pluck('id')->first() : Category::factory()->expense()->create(['company_id' => $this->company->id])->id;
+            $expense_category_id = $expense_categories->count() ? $expense_categories->random(1)->pluck('id')->first() : \App\Models\Setting\Category::factory()->expense()->create(['company_id' => $this->company->id])->id;
 
             return [
                 'type' => Model::BILL_TYPE,
